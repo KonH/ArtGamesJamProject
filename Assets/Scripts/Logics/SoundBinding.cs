adfa562508d8dd23f514563b599831da9619e462
KonH <konh@yandex.ru>
2017-10-07 15:50:07 +0700

    Game end sound;

--- Assets/Scripts/Logics/SoundBinding.cs
+++ Assets/Scripts/Logics/SoundBinding.cs
@@ -8,17 +8,22 @@ using UDBase.Controllers.SoundSystem;
 public class SoundBinding : MonoBehaviour {
 	
 	public AudioClipHolder Click;
+	public AudioClipHolder End;
 
 	void Start() {
 		Events.Subscribe<Audio_Click>(this, OnClick);
+		Events.Subscribe<Game_End>(this, OnEnd);
 	}
 
 	void OnDestroy() {
-		Events.Unsubscribe<Audio_Click>(OnClick);
+		Events.Unsubscribe<Game_End>(OnEnd);
 	}
 
 	void OnClick(Audio_Click e) {
-		Debug.Log("ON_CLICK");
 		Sound.Play(Click.Id);
+	}
+
+	void OnEnd(Game_End e) {
+		Sound.Play(End.Id);
 	}
 }
